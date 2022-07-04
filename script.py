8cf37d2fa1b6db71f63444a769ed307d722a3911
Ediel <elopez69@ucmerced.edu>
2022-07-04 11:54:02 -0700

    working version

--- script.py
+++ script.py
@@ -1,11 +1,76 @@
+# Imports modules
 import variables        # Stores my Google Email & Password
-import requests                           # Web Scraping
-from bs4 import BeautifulSoup             # Web Scraping
-import smtplib                            # Send Email
-from email.message import EmailMessage    # Send Email
 from twilio.rest import Client            # Twilio
-from datetime import datetime             # Display Time
-import time                               # Go To Sleep
+import RPi.GPIO as GPIO
+from datetime import datetime
+from time import sleep
+
+# Static program vars
+pin = 5  # Input pin of sensor (GPIO.BOARD)
+Buttons = [0x300fd00ff, 0x300fd807f, 0x300fd40bf, 0x300fd20df, 0x300fda05f, 0x300fd609f, 0x300fd10ef, 0x300fd906f, 0x300fd50af, 0x300fd30cf, 0x300fdb04f, 0x300fd708f, 0x300fd08f7, 0x300fd8877, 0x300fd48b7, 0x300fd28d7, 0x300fda857, 0x300fd6897, 0x300fd18e7, 0x300fd9867, 0x300fd58a7]  # HEX code list
+ButtonsNames = ["volumeDown", "playPause", "volumeUp", "setup", "previous", "stopMode", "channelDown", "enterSave", "channelNext", "zeroTen", "next", "previous", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]# String list in same order as HEX list
+
+# Sets up GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN)
+
+# Gets binary value
+def getBinary():
+    # Internal vars
+    num1s = 0  # Number of consecutive 1s read
+    binary = 1  # The binary value
+    command = []  # The list to store pulse times in
+    previousValue = 0  # The last value
+    value = GPIO.input(pin)  # The current value
+
+    # Waits for the sensor to pull pin low
+    while value:
+        sleep(0.0001) # This sleep decreases CPU utilization immensely
+        value = GPIO.input(pin)
+        
+    # Records start time
+    startTime = datetime.now()
+    
+    while True:
+        # If change detected in value
+        if previousValue != value:
+            now = datetime.now()
+            pulseTime = now - startTime #Calculate the time of pulse
+            startTime = now #Reset start time
+            command.append((previousValue, pulseTime.microseconds)) #Store recorded data
+            
+        # Updates consecutive 1s variable
+        if value:
+            num1s += 1
+        else:
+            num1s = 0
+        
+        # Breaks program when the amount of 1s surpasses 10000
+        if num1s > 10000:
+            break
+            
+        # Re-reads pin
+        previousValue = value
+        value = GPIO.input(pin)
+        
+    # Converts times to binary
+    for (typ, tme) in command:
+        if typ == 1: #If looking at rest period
+            if tme > 1000: #If pulse greater than 1000us
+                binary = binary *10 +1 #Must be 1
+            else:
+                binary *= 10 #Must be 0
+            
+    if len(str(binary)) > 34: #Sometimes, there is some stray characters
+        binary = int(str(binary)[:34])
+        
+    return binary
+    
+# Convert value to hex
+def convertHex(binaryValue):
+    tmpB2 = int(str(binaryValue),2) #Temporarely propper base 2
+    return hex(tmpB2)
+
 
 
 def send_email(subject, recipient, message):
@@ -31,28 +96,17 @@ def send_sms(recipient, message):
     return
 
 
-if __name__ == "__main__":
-    while True:
-        try:
-            message = datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + ": Test Message"
-
-            send_sms(variables.number_ediel, message)
-
-            time.sleep(30)  # 30 seconds
-            # =======================================================================
-        except requests.exceptions.ConnectionError as errc:
-            # In the event of a network problem (e.g. DNS failure, refused connection, etc),
-            # Requests will raise a ConnectionError exception.
-            print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.")
-            print(errc.__str__())
-
-            # Sleep for 1 minute and try to run the program again.
-            time.sleep(60)  # 1 minute
-            continue
-        except Exception as exc:
-            # This block will execute when an unexpected error that is unrelated to connection error occurs.
-            errorMessage = "AN ERROR OCCURRED AT " + datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + "!\n"
-            print(errorMessage + "Here is the specific error:\n" + exc.__str__())
-
-            send_sms(variables.number_ediel, errorMessage)
-            break
+while True:
+    inData = convertHex(getBinary()) #Runs subs to get incoming hex value
+    for button in range(len(Buttons)):#Runs through every value in list
+        if hex(Buttons[button]) == inData: #Checks this against incoming
+            print(ButtonsNames[button]) #Prints corresponding english name for button
+            if (str(ButtonsNames[button]) == 'one'):
+                message = "I need help"
+                send_sms(variables.number_ediel, message)
+            elif (str(ButtonsNames[button]) == 'two'):
+                message = "Call me"
+                send_sms(variables.number_ediel, message)
+            elif (str(ButtonsNames[button]) == 'three'):
+                message = "YouTube"
+                send_sms(variables.number_ediel, message)
---
+++ variables.py
@@ -0,0 +1,8 @@
+EMAIL_USER = ''
+EMAIL_TO_EDIEL = ''
+EMAIL_PASS = ''
+
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
+number_twilio = ""
+number_ediel = ""
